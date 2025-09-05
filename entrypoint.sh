dca3cfb5873e9ca3ecd822f26bc594cb6811b5d8
Richard Franks <richard.franks@securenetcoms.co.uk>
2025-09-05 13:58:29 +0000

    Removed chown, changed to run as current user

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-chown -R nobody:nogroup /etc/serles || true
-
-runuser -u nobody -- /serles/bin/gunicorn -c /serles/gunicorn_config.py "serles:create_app()"
+/serles/bin/gunicorn -c /serles/gunicorn_config.py "serles:create_app()"
